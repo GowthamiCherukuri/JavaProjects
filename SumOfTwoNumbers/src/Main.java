2a71c3ad5078ea0783eebf3256aae2f97039f7bf
GowthamiCherukuri <gowthamirushitha12@gmail.com>
2023-09-10 15:09:32 -0700

    added logic to add two numbers

--- SumOfTwoNumbers/src/Main.java
+++ SumOfTwoNumbers/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hi! This is my first java code");
+        System.out.println("Enter two number to get the sum");
+        Scanner readInput = new Scanner(System.in);
+        int inputA = readInput.nextInt();
+        int inputB = readInput.nextInt();
+        System.out.print("Sum of the two numbers is : ");
+        System.out.print(inputA + inputB);
     }
 }
